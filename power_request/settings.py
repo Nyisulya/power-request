f10144aaa00705f5c1be98467200bdc67f2cf8c3
Nyisulya <nyisulya@users.noreply.github.com>
2026-07-17 06:59:54 -0700

    Configure DEFAULT_AUTO_FIELD to silence auto-created primary key warnings

--- power_request/settings.py
+++ power_request/settings.py
@@ -140,3 +140,7 @@ STATIC_URL = 'static/'
 # Media files (Uploaded images/files)
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
